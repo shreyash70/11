fd356b215312068efb22c00fd99cc03bbf64858a
shreyash70 <87362534+shreyash70@users.noreply.github.com>
2021-11-21 16:06:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+console.log("My first console project");
+
+var first_name = "Ranbir";
+var last_name = "Kapoor";
+
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a = 10;
+var b = 5;
+
+
+var add = a+b ;
+console.log(add);
+
+var sub = a-b ;
+console.log(sub);
+
+var mutiple = a*b ;
+console.log(mutiple);
+
+var divide = a \ b ;
+console.log(divide);
